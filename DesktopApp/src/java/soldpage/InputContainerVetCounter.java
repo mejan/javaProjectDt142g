ec707015e75a9ea3b695c953fec77d30add4d990
mejan <najemes0@gmail.com>
2014-10-09 20:08:20 +0200

    Update container class implementeted.

--- DesktopApp/src/java/soldpage/InputContainerVetCounter.java
+++ DesktopApp/src/java/soldpage/InputContainerVetCounter.java
@@ -15,6 +15,36 @@ public class InputContainerVetCounter {
         date="";
         price=0;
     }
+    
+    InputContainerVetCounter(String pro, double pri, String da){
+        product = pro;
+        date = da;
+        price = pri;
+    }
+    
+    void setProduct(String pro){
+        product = pro;
+    }
+    
+    void setDate(String da){
+        date = da;
+    }
+    
+    void setPrice(double pri){
+        price=pri;
+    }
+    
+    String getProduct(){
+        return product;
+    }
+    
+    String getDate(){
+        return date;
+    }
+    
+    double getPrice(){
+        return price;
+    }
 
     private String product;
     private String date;
