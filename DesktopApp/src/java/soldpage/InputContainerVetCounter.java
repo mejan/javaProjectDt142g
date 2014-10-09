63e3c8c5d4b9fa61f720d7afe12c2c34f394406d
mejan <najemes0@gmail.com>
2014-10-09 20:00:16 +0200

    Container for vetcounter

---
+++ DesktopApp/src/java/soldpage/InputContainerVetCounter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soldpage;
+
+/**
+ *
+ * @author mejan
+ */
+public class InputContainerVetCounter {
+    
+    InputContainerVetCounter(){
+        product="";
+        date="";
+        price=0;
+    }
+
+    private String product;
+    private String date;
+    private double price;
+}
