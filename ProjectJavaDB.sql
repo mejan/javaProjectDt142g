936aa1e6b27d0670fe4828c2bc366df4057bebfa
mejan <najemes0@gmail.com>
2014-10-16 11:13:39 +0200

    tog bort gammal databas och la till ny

--- ProjectJavaDB.sql
+++ ProjectJavaDB.sql
@@ -2,10 +2,10 @@
 -- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
--- Värd: localhost
--- Tid vid skapande: 15 okt 2014 kl 14:14
--- Serverversion: 5.6.20
--- PHP-version: 5.5.15
+-- Host: 127.0.0.1
+-- Generation Time: Oct 15, 2014 at 02:37 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,305 +17,264 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Databas: `ProjectJavaDB`
+-- Database: `projectjavadb`
 --
-CREATE DATABASE IF NOT EXISTS `ProjectJavaDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-USE `ProjectJavaDB`;
+CREATE DATABASE IF NOT EXISTS `projectjavadb` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `projectjavadb`;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `Consultation`
+-- Table structure for table `about`
 --
 
-CREATE TABLE IF NOT EXISTS `Consultation` (
+CREATE TABLE IF NOT EXISTS `about` (
+`id` int(11) NOT NULL,
+  `about` text NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `about`) VALUES
+(1, 'Det här är en kort beskrivning av företaget: Anton''s gitarrverkstad!\r\n\r\nGitarrer\r\nTrök\r\nHejs');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE IF NOT EXISTS `company` (
+  `name` varchar(250) NOT NULL,
+  `city` varchar(250) NOT NULL,
+  `street` varchar(250) NOT NULL,
+  `areaCode` int(10) NOT NULL,
+  `phone` int(25) NOT NULL,
+  `email` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`name`, `city`, `street`, `areaCode`, `phone`, `email`) VALUES
+('Anton''s gitarrverkstad', 'Sundsvall', 'Bergsgatan 14', 85352, 123456, 'anton@gitarrverkstad.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `consultation`
+--
+
+CREATE TABLE IF NOT EXISTS `consultation` (
 `consultationID` int(10) NOT NULL,
   `customerID` int(10) NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL,
   `description` varchar(100) COLLATE utf8_bin NOT NULL,
   `accepted` tinyint(1) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=30 ;
 
 --
--- Dumpning av Data i tabell `Consultation`
+-- Dumping data for table `consultation`
 --
 
-INSERT INTO `Consultation` (`consultationID`, `customerID`, `date`, `time`, `description`, `accepted`) VALUES
-(1, 7, '2014-10-02', '09:00:34', 'keso', 1),
-(4, 12, '2014-10-02', '09:08:24', 'Jag är troende', 0),
-(7, 20, '2014-10-02', '09:57:11', 'Jag luktar jude', 0),
-(8, 19, '2014-10-02', '09:58:25', 'dgfhsdgfhsdg', 0),
-(9, 21, '2014-10-02', '14:10:19', 'Min gitarr spricker nÃ¤r jag smeker den med min penis.', 0),
-(10, 21, '2014-10-02', '14:14:59', 'Min gitarr spricker nÃ¤r jag smeker den med min penis.', 0),
-(11, 21, '2014-10-02', '14:15:35', 'regeN baklänges?', 0),
-(12, 22, '2014-10-02', '17:19:46', 'helangÃ¥Ã¥r', 0),
-(13, 23, '2014-10-09', '11:23:29', 'fghjh', 0);
+INSERT INTO `consultation` (`consultationID`, `customerID`, `date`, `time`, `description`, `accepted`) VALUES
+(20, 27, '2014-10-05', '16:18:50', 'Robin1', 0),
+(21, 28, '2014-10-05', '16:19:19', 'Max1', 0),
+(22, 27, '2014-10-05', '16:19:51', 'Robin2', 0),
+(23, 29, '2014-10-05', '16:22:37', 'lol', 0),
+(24, 31, '2014-10-05', '16:23:57', 'henke1', 0),
+(25, 31, '2014-10-05', '16:25:18', 'henke2', 0),
+(26, 32, '2014-10-05', '16:27:47', 'asdasd', 0),
+(27, 33, '2014-10-07', '21:29:30', 'd', 0),
+(28, 33, '2014-10-13', '19:57:04', 'd', 0),
+(29, 33, '2014-10-13', '19:58:04', 'd', 0);
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `Customer`
+-- Table structure for table `customer`
 --
 
-CREATE TABLE IF NOT EXISTS `Customer` (
+CREATE TABLE IF NOT EXISTS `customer` (
 `customerID` int(10) NOT NULL,
   `name` varchar(15) COLLATE utf8_bin NOT NULL,
   `lastName` varchar(15) COLLATE utf8_bin NOT NULL,
   `email` varchar(35) COLLATE utf8_bin NOT NULL,
   `phoneNumber` varchar(15) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=34 ;
 
 --
--- Dumpning av Data i tabell `Customer`
+-- Dumping data for table `customer`
 --
 
-INSERT INTO `Customer` (`customerID`, `name`, `lastName`, `email`, `phoneNumber`) VALUES
-(3, 'Anton', 'Dahlin', 'anton.dahlin@outlook.com', '0738187707'),
-(7, 'keso', 'keso', 'keso', 'keso'),
-(9, 'Christoffer', 'KUKEN', 'kuken@keso.com', '3245678'),
-(11, 'Pontus', 'Gårdinger', 'gårdi@negress.com', '32453678'),
-(12, 'Pontus', 'Ekholm', 'poek1101@student.miun.se', '324564756432'),
-(18, 'hihi', 'hihi', 'hihi', 'hihi'),
-(19, 'Fnissah', 'Skratt', '@.com', '3245678'),
-(20, 'dfgh', 'dsfghj', 'gfdshjuiyurtjh', 'fdghjkfgjhjd'),
-(21, 'Robin', 'Jansson', 'robin.jansson@hotmale.com', '0702190262'),
-(22, 'Keso', 'Balle', 'jesus@lever.se', 'r2435646'),
-(23, 'Kajsa', 'bergkvist', '@kes.com', '456378436');
+INSERT INTO `customer` (`customerID`, `name`, `lastName`, `email`, `phoneNumber`) VALUES
+(27, 'Robin', 'Jansson', 'RobinJansson@hotmail.com', '123'),
+(28, 'Max', 'Alenius', 'MaxAlenius@hotmail.com', '456'),
+(29, 'a', 'b', 'ad@aasd.se', 'c'),
+(31, 'asd', 'asd', 'henke', 'asd'),
+(32, 'l', 'l', 'lolasdsadsad', 'l'),
+(33, 'a', 'a', 'c', 'b');
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `Guitars`
+-- Table structure for table `guitars`
 --
 
-CREATE TABLE IF NOT EXISTS `Guitars` (
-`guitarrID` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `guitars` (
+  `guitarrID` int(10) NOT NULL,
   `name` varchar(15) COLLATE utf8_bin NOT NULL,
   `price` int(10) NOT NULL,
   `description` varchar(100) COLLATE utf8_bin NOT NULL,
   `sold` tinyint(1) NOT NULL,
   `imagePath` varchar(100) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
-
---
--- Dumpning av Data i tabell `Guitars`
---
-
-INSERT INTO `Guitars` (`guitarrID`, `name`, `price`, `description`, `sold`, `imagePath`) VALUES
-(2, 'Gibson 1337', 4000, 'Jesus lever', 0, 'resources/image/gibson1.png');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `News`
+-- Table structure for table `news`
 --
 
-CREATE TABLE IF NOT EXISTS `News` (
+CREATE TABLE IF NOT EXISTS `news` (
 `newsID` int(11) NOT NULL,
-  `newsText` varchar(150) COLLATE utf8_bin NOT NULL,
-  `newsImage` varchar(250) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=5 ;
+  `newsText` varchar(250) NOT NULL,
+  `newsImage` varchar(250) NOT NULL,
+  `newsName` varchar(250) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Dumpning av Data i tabell `News`
+-- Dumping data for table `news`
 --
 
-INSERT INTO `News` (`newsID`, `newsText`, `newsImage`) VALUES
-(1, 'brandeby', 'resources/image/small1.png'),
-(2, 'Kvarg', 'resources/image/small1.png'),
-(3, 'kajsa', 'resources/image/small1.png'),
-(4, 'legolas', 'resources/image/small1.png');
+INSERT INTO `news` (`newsID`, `newsText`, `newsImage`, `newsName`) VALUES
+(1, 'Det här är artikel nummer ett, LOREM IPSUM DELOOOOORORERER', 'resources/image/small1.png', 'Iphone 6 review'),
+(2, 'This is article nr 2', 'resources/image/small1.png', 'Article nr 2'),
+(3, 'Haha this is so cool', 'resources/image/small1.png', 'NO not again'),
+(4, 'sadsadsad', 'resources/image/small1.png', 'asdadsadad');
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `ReparationPictures`
+-- Table structure for table `reparations`
 --
 
-CREATE TABLE IF NOT EXISTS `ReparationPictures` (
-`pictureID` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `reparations` (
   `repairID` int(10) NOT NULL,
-  `imagePath` varchar(30) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
-
---
--- Dumpning av Data i tabell `ReparationPictures`
---
-
-INSERT INTO `ReparationPictures` (`pictureID`, `repairID`, `imagePath`) VALUES
-(2, 1, '/source/image'),
-(3, 1, '/source/image2');
-
--- --------------------------------------------------------
-
---
--- Tabellstruktur `Reparations`
---
-
-CREATE TABLE IF NOT EXISTS `Reparations` (
-`repairID` int(10) NOT NULL,
   `customerID` int(10) NOT NULL,
   `hoursWorked` int(11) NOT NULL,
   `imagePath` varchar(60) COLLATE utf8_bin NOT NULL,
   `recieved` date NOT NULL,
   `done` date NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=21 ;
-
---
--- Dumpning av Data i tabell `Reparations`
---
-
-INSERT INTO `Reparations` (`repairID`, `customerID`, `hoursWorked`, `imagePath`, `recieved`, `done`) VALUES
-(1, 3, -4003, '/src/keso', '2014-10-08', '2014-10-31'),
-(19, 20, 4, 'resources/image/regeN.png', '2014-10-14', '2014-10-14'),
-(20, 20, 4, 'resources/image/regeN.png', '2014-10-14', '2014-10-14');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `SoldItems`
+-- Table structure for table `spareparts`
 --
 
-CREATE TABLE IF NOT EXISTS `SoldItems` (
-`soldID` int(11) NOT NULL,
-  `itemName` varchar(40) COLLATE utf8_bin NOT NULL,
-  `dateOfSale` date NOT NULL,
-  `price` double NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=39 ;
-
---
--- Dumpning av Data i tabell `SoldItems`
---
-
-INSERT INTO `SoldItems` (`soldID`, `itemName`, `dateOfSale`, `price`) VALUES
-(1, 'Strängar', '2014-10-12', 100.5),
-(30, 'kräfts', '1987-12-28', 3456);
-
--- --------------------------------------------------------
-
---
--- Tabellstruktur `SpareParts`
---
-
-CREATE TABLE IF NOT EXISTS `SpareParts` (
+CREATE TABLE IF NOT EXISTS `spareparts` (
   `spareID` varchar(10) COLLATE utf8_bin NOT NULL,
   `name` varchar(20) COLLATE utf8_bin NOT NULL,
   `price` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Index för dumpade tabeller
+-- Indexes for dumped tables
 --
 
 --
--- Index för tabell `Consultation`
+-- Indexes for table `about`
 --
-ALTER TABLE `Consultation`
+ALTER TABLE `about`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+ ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `consultation`
+--
+ALTER TABLE `consultation`
  ADD PRIMARY KEY (`consultationID`), ADD KEY `FK_consultation` (`customerID`);
 
 --
--- Index för tabell `Customer`
+-- Indexes for table `customer`
 --
-ALTER TABLE `Customer`
+ALTER TABLE `customer`
  ADD PRIMARY KEY (`customerID`);
 
 --
--- Index för tabell `Guitars`
+-- Indexes for table `guitars`
 --
-ALTER TABLE `Guitars`
+ALTER TABLE `guitars`
  ADD PRIMARY KEY (`guitarrID`);
 
 --
--- Index för tabell `News`
+-- Indexes for table `news`
 --
-ALTER TABLE `News`
+ALTER TABLE `news`
  ADD PRIMARY KEY (`newsID`);
 
 --
--- Index för tabell `ReparationPictures`
+-- Indexes for table `reparations`
 --
-ALTER TABLE `ReparationPictures`
- ADD PRIMARY KEY (`pictureID`), ADD KEY `FK_reparationPictures` (`repairID`);
-
---
--- Index för tabell `Reparations`
---
-ALTER TABLE `Reparations`
+ALTER TABLE `reparations`
  ADD PRIMARY KEY (`repairID`), ADD KEY `FK_reparations` (`customerID`);
 
 --
--- Index för tabell `SoldItems`
+-- Indexes for table `spareparts`
 --
-ALTER TABLE `SoldItems`
- ADD PRIMARY KEY (`soldID`);
-
---
--- Index för tabell `SpareParts`
---
-ALTER TABLE `SpareParts`
+ALTER TABLE `spareparts`
  ADD PRIMARY KEY (`spareID`);
 
 --
--- AUTO_INCREMENT för dumpade tabeller
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT för tabell `Consultation`
+-- AUTO_INCREMENT for table `about`
 --
-ALTER TABLE `Consultation`
-MODIFY `consultationID` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+ALTER TABLE `about`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT för tabell `Customer`
+-- AUTO_INCREMENT for table `consultation`
 --
-ALTER TABLE `Customer`
-MODIFY `customerID` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+ALTER TABLE `consultation`
+MODIFY `consultationID` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
 --
--- AUTO_INCREMENT för tabell `Guitars`
+-- AUTO_INCREMENT for table `customer`
 --
-ALTER TABLE `Guitars`
-MODIFY `guitarrID` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+ALTER TABLE `customer`
+MODIFY `customerID` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
 --
--- AUTO_INCREMENT för tabell `News`
+-- AUTO_INCREMENT for table `news`
 --
-ALTER TABLE `News`
+ALTER TABLE `news`
 MODIFY `newsID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT för tabell `ReparationPictures`
---
-ALTER TABLE `ReparationPictures`
-MODIFY `pictureID` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT för tabell `Reparations`
---
-ALTER TABLE `Reparations`
-MODIFY `repairID` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
---
--- AUTO_INCREMENT för tabell `SoldItems`
---
-ALTER TABLE `SoldItems`
-MODIFY `soldID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=39;
---
--- Restriktioner för dumpade tabeller
+-- Constraints for dumped tables
 --
 
 --
--- Restriktioner för tabell `Consultation`
+-- Constraints for table `consultation`
 --
-ALTER TABLE `Consultation`
-ADD CONSTRAINT `FK_consultation` FOREIGN KEY (`customerID`) REFERENCES `Customer` (`customerID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `consultation`
+ADD CONSTRAINT `FK_consultation` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Restriktioner för tabell `ReparationPictures`
+-- Constraints for table `reparations`
 --
-ALTER TABLE `ReparationPictures`
-ADD CONSTRAINT `FK_reparationPictures` FOREIGN KEY (`repairID`) REFERENCES `Reparations` (`repairID`);
-
---
--- Restriktioner för tabell `Reparations`
---
-ALTER TABLE `Reparations`
-ADD CONSTRAINT `FK_reparations` FOREIGN KEY (`customerID`) REFERENCES `Customer` (`customerID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `reparations`
+ADD CONSTRAINT `FK_reparations` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
